76293870476240780866a143ce887582db91d145
Kashish Bhasin <kashishbhasin6@gmail.com>
2026-04-25 13:34:51 +0530

    7 commit

--- backend/server.js
+++ backend/server.js
@@ -112,3 +112,8 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
+app.get("/test", (req, res) => {
+  res.send("Backend is working ✅");
+});
